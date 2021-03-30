a07f0aefd904451925798560146e94a08590eb4b
Felipe Bezerra <fbezerra.tsi@gmail.com>
2021-03-30 17:33:14 -0300

    Revert "Autenticaçao de usuario"
    This reverts commit 14c60c6dd1f03abb48ceec8f92e1b7cd007b6a53.

--- src/config/middlewares.js
+++
@@ -1,8 +0,0 @@
-const bodyParser = require('body-parser')
-const cors = require('cors')
-
-/** @param { import('express').Express} app */
-module.exports = app => {
-    app.use(bodyParser.json())
-    app.use(cors())
-}
