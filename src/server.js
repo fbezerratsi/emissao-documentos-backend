1bcf4dfb37c671a02ac05b5cbb3b5a62518ae099
Felipe Bezerra <fbezerra.tsi@gmail.com>
2021-03-30 18:11:16 -0300

    Configurar porta dop heroku

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ require('./config/db')
 app.use(express.json())
 app.use(routes)
 app.disable('x-powered-by') // Remover da resposta HTTP (header: x-powered-by) a referência de que o Express/Node compõem a lista de tecnologias utilizadas, isso irá afastar rotinas mais simples de varredura e ataques automatizados
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Executando...') 
 })
 
